d42222a6c357fe24d67df98a6a8ee6bf93ac6493
Abhaym249 <abhaym249@gmail.com>
2026-02-02 14:35:57 +0530

    added controller and view in this

---
+++ MVC_codefirst_101/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using MVC_codefirst_101.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_codefirst_101.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationdbContext context;
+        public EmployeeController()
+            {
+                context = new ApplicationdbContext();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employeesList = context.Employees.ToList();
+
+            return View(employeesList);
+        }
+    }
+}
