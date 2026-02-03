3a37eeabc7e863bf96d6d1a3b9f89c714966fde8
Abhaym249 <abhaym249@gmail.com>
2026-02-03 20:11:46 +0530

    added logic behind create data  otpion adn some changes in view

--- MVC_codefirst_101/Controllers/EmployeeController.cs
+++ MVC_codefirst_101/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC_codefirst_101.Data;
+using MVC_codefirst_101.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,8 @@ namespace MVC_codefirst_101.Controllers
     {
         private readonly ApplicationdbContext context;
         public EmployeeController()
-            {
-                context = new ApplicationdbContext();
+        {
+            context = new ApplicationdbContext();
         }
         // GET: Employee
         public ActionResult Index()
@@ -20,6 +21,24 @@ namespace MVC_codefirst_101.Controllers
             var employeesList = context.Employees.ToList();
 
             return View(employeesList);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (employee == null) return HttpNotFound();
+
+            context.Employees.Add(employee);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id) {
+            var employee = context.Employees.Find(id);
+            if (employee == null) return HttpNotFound();
+            return View(employee);
         }
     }
 }
