80e19a6d0562b07400aa3da7404e61d34152b63f
Abhaym249 <abhaym249@gmail.com>
2026-02-04 19:07:13 +0530

    added logic to edit details and delete employee

--- MVC_codefirst_101/Controllers/EmployeeController.cs
+++ MVC_codefirst_101/Controllers/EmployeeController.cs
@@ -1,9 +1,6 @@
 ﻿using MVC_codefirst_101.Data;
 using MVC_codefirst_101.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVC_codefirst_101.Controllers
@@ -35,10 +32,46 @@ namespace MVC_codefirst_101.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
-        public ActionResult Edit(int id) {
+        public ActionResult Edit(int id)
+        {
             var employee = context.Employees.Find(id);
             if (employee == null) return HttpNotFound();
             return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (employee == null) return HttpNotFound();
+            var emloyeefromDb = context.Employees.Find(employee.Id);
+            if (emloyeefromDb == null) return HttpNotFound();
+            emloyeefromDb.Name = employee.Name;
+            emloyeefromDb.Address = employee.Address;
+            emloyeefromDb.Salary = employee.Salary;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var employeefromDb = context.Employees.Find(id);
+            if (employeefromDb == null) return HttpNotFound();
+            return View(employeefromDb);
+
+        }
+        public ActionResult Delete(int id)
+        {
+            var employeefromDb = context.Employees.Find(id);
+            if (employeefromDb == null) return HttpNotFound();
+            return View(employeefromDb);
+        }
+        [HttpPost]
+        public ActionResult Delete(Employee employee)
+        {
+            if (employee == null) return HttpNotFound();
+            var employeefromDb = context.Employees.Find(employee.Id);
+            if (employeefromDb == null) return HttpNotFound();
+            context.Employees.Remove(employeefromDb);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
